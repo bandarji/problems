2d1b97a4eb217889f509c119bff230b1c7cd65e0
Sean Jain Ellis <sellis@bandarji.com>
2024-07-07 14:06:04 -1000

    Go fizz buzz

---
+++ leetcode/fizz_buzz/fizz_buzz.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func fizzBuzz(n int) []string {
+	fb := []string{}
+	tmp := ""
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 {
+			tmp += "Fizz"
+		}
+		if i%5 == 0 {
+			tmp += "Buzz"
+		}
+		if len(tmp) == 0 {
+			tmp = fmt.Sprintf("%d", i)
+		}
+		fb = append(fb, tmp)
+		tmp = ""
+	}
+	return fb
+}
+
+func main() {
+	fmt.Println(fizzBuzz(15))
+}
