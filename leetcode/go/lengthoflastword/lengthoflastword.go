840673b2fef961c0de17fa146c701940001ad6e2
Sean Jain Ellis <sellis@bandarji.com>
2025-02-02 11:12:36 -0800

    lenlastword

---
+++ leetcode/go/lengthoflastword/lengthoflastword.go
@@ -0,0 +1,15 @@
+package lengthoflastword
+
+func lengthOfLastWord(s string) int {
+	length := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			if length == 0 {
+				continue
+			}
+			break
+		}
+		length++
+	}
+	return length
+}
---
+++ leetcode/go/lengthoflastword/lengthoflastword_test.go
@@ -0,0 +1,25 @@
+package lengthoflastword
+
+import "testing"
+
+func Test_lengthOfLastWord(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case 1", args{"Hello World"}, 5},
+		{"case 2", args{"   fly me   to   the moon  "}, 4},
+		{"case 3", args{"luffy is still joyboy"}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWord(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
